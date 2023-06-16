34b1ec4f31ad203bcd74d89ab1e8b1aa1ba6ba69
namnh198 <namnhn98@gmail.com>
2023-06-17 01:10:49 +0700

    remove loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,9 @@
 import "@/styles/styles.scss";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Aos from "aos";
 import { ThemeProvider } from "next-themes";
-import { Loader } from "@/components/templates";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-
   useEffect(() => {
     Aos.init({
       duration: 1500,
@@ -16,26 +11,9 @@ export default function App({ Component, pageProps }) {
     });
   }, []);
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      setLoading(true);
-    };
-
-    const handleRouteChangeComplete = () => {
-      setLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleRouteChange);
-    router.events.on("routeChangeComplete", handleRouteChangeComplete);
-
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-      router.events.off("routeChangeComplete", handleRouteChangeComplete);
-    };
-  }, [router.events]);
   return (
     <ThemeProvider defaultTheme="light">
-      {loading ? <Loader /> : <Component {...pageProps} />}
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
